9528e1f880eb0536f41782897624f139e396d7e8
Hariharan482 <125631751+Hariharan482@users.noreply.github.com>
2023-03-03 20:24:08 +0530

    Update FlyWeight.java

--- Exercise5/FlyWeight.java
+++ Exercise5/FlyWeight.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class FlyWeight {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Factory factory=new Factory();
 		Scanner scanner=new Scanner(System.in);
 		String name=scanner.next();
